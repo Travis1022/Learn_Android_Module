54e682a9415876ddd5d2f0d56f43cc4c826c8349
Travis125 <1287200169@qq.com>
2017-02-10 17:06:15 +0800

    JPush

--- JPushDemo/app/src/main/java/com/jpush/JPushReceiver.java
+++ JPushDemo/app/src/main/java/com/jpush/JPushReceiver.java
@@ -46,12 +46,12 @@ public class JPushReceiver extends BroadcastReceiver {
         else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
             Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
 
-           /* //打开自定义的Activity
+            //打开自定义的Activity
             Intent i = new Intent(context, TestActivity.class);
             i.putExtras(bundle);
             //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            context.startActivity(i);*/
+            context.startActivity(i);
 
         }
         //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
@@ -101,6 +101,28 @@ public class JPushReceiver extends BroadcastReceiver {
             }
         }
         return sb.toString();
+    }
+
+    //send msg to MainActivity
+    private void processCustomMessage(Context context, Bundle bundle) {
+        if (MainActivity.isForeground) {
+            String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+            String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            Intent msgIntent = new Intent(MainActivity.MESSAGE_RECEIVED_ACTION);
+            msgIntent.putExtra(MainActivity.KEY_MESSAGE, message);
+            if (!ExampleUtil.isEmpty(extras)) {
+                try {
+                    JSONObject extraJson = new JSONObject(extras);
+                    if (extraJson.length() > 0) {
+                        msgIntent.putExtra(MainActivity.KEY_EXTRAS, extras);
+                    }
+                } catch (JSONException e) {
+
+                }
+
+            }
+            context.sendBroadcast(msgIntent);
+        }
     }
 
 }
